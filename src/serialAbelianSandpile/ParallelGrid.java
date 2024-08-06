183ae4c48a14ee85f30fc51f6b5ede86385d217d
Kavya Kaushik <kavyaashi10@gmail.com>
2024-08-06 19:51:59 +0200

    Parallel algorithms updated on Parakllel grid

--- src/serialAbelianSandpile/ParallelGrid.java
+++ src/serialAbelianSandpile/ParallelGrid.java
@@ -16,20 +16,28 @@ class ParallelGrid extends RecursiveAction{
     }
 
     protected void compute(){
-        for (int i = start; i<end; i++){
-            for(int j = 1; j< grid.getColumns()+i;j++){
-                grid.updateGrid[i][j] = (grid.get(i, j) % 4) +
-                                        (grid.get(i - 1, j) / 4) +
-                                        (grid.get(i + 1, j) / 4) +
-                                        (grid.get(i, j - 1) / 4) +
-                                        (grid.get(i, j + 1) / 4);
+
+        if ((end - start)<=5){
+            //process directly for a small task 
+            for (int i = start; i<end; i++){
+                for(int j = 1; j< grid.getColumns()+i;j++){
+                    grid.updateGrid[i][j] = (grid.get(i, j) % 4) +
+                                            (grid.get(i - 1, j) / 4) +
+                                            (grid.get(i + 1, j) / 4) +
+                                            (grid.get(i, j - 1) / 4) +
+                                            (grid.get(i, j + 1) / 4);
 
 
+                }
             }
-
-
+        }else {
+        //splitting the task
+        int mid = (start + end) / 2;
+        ParallelGrid grid1 = new ParallelGrid(grid, start, mid);
+        ParallelGrid grid2 = new ParallelGrid(grid, start, mid);
+        grid1.fork();
+        grid2.compute();
+        grid1.join();
         }
-
     }
-
 }
