a9a4da6bda3c1f4f03d4da2b96da8e6a315ae066
Kavya Kaushik <kavyaashi10@gmail.com>
2024-08-10 13:32:12 +0200

    - Imporved forkjoin pool implementation

--- src/serialAbelianSandpile/ParallelAutomatonSimulation.java
+++ src/serialAbelianSandpile/ParallelAutomatonSimulation.java
@@ -1,57 +1,52 @@
 package serialAbelianSandpile;
 
-
 import java.io.IOException;
-import serialAbelianSandpile.AutomatonSimulation;
-import serialAbelianSandpile.Grid;
-
+import java.util.concurrent.ForkJoinPool;
 
 public class ParallelAutomatonSimulation extends AutomatonSimulation {
 
-
-    //time
     static long startTime = 0;
     static long endTime = 0;
 
-
-    //timers - note milliseconds
     private static void tick(){ //start timing
         startTime = System.currentTimeMillis();
     }
-    private static void tock(){ //end timing
-        endTime=System.currentTimeMillis();
-    }
-    //time
 
+    private static void tock(){ //end timing
+        endTime = System.currentTimeMillis();
+    }
 
     public static void main(String[] args) throws IOException {
-        Grid simulationGrid;
-
-
         if (args.length != 2) {
             System.out.println("Incorrect number of command line arguments provided.");
             System.exit(0);
         }
 
-
         String inputFileName = args[0];
         String outputFileName = args[1];
 
+        // Initialize the grid
+        Grid simulationGrid = new ParallelGrid(readArrayFromCSV(inputFileName));
 
-        simulationGrid = new ParallelGrid(readArrayFromCSV(inputFileName));
+        // Create the ForkJoinPool once in the simulation class
+        ForkJoinPool pool = new ForkJoinPool();
+
         int counter = 0;
         tick();
-        simulationGrid.update()
+
+
+        // Use the same pool for all updates
+        while (((ParallelGrid) simulationGrid).update(pool)) {
+            counter++;
+        }
         tock();
 
+        pool.shutdown(); // Shutdown the pool when done
 
         System.out.println("Simulation complete, writing image...");
         simulationGrid.gridToImage(outputFileName);
-
 
         System.out.printf("Number of steps to stable state: %d \n", counter);
         System.out.printf("Time: %d ms\n", endTime - startTime);
     }
 }
-
-
