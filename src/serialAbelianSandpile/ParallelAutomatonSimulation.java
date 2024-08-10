bfeffaaacefb4c688ed37eba25b5b36ab5b73b33
Kavya Kaushik <kavyaashi10@gmail.com>
2024-08-10 10:23:05 +0200

    - Debugging parallel automaton

--- src/serialAbelianSandpile/ParallelAutomatonSimulation.java
+++ src/serialAbelianSandpile/ParallelAutomatonSimulation.java
@@ -1,53 +1,57 @@
 package serialAbelianSandpile;
 
+
 import java.io.IOException;
 import serialAbelianSandpile.AutomatonSimulation;
 import serialAbelianSandpile.Grid;
 
+
 public class ParallelAutomatonSimulation extends AutomatonSimulation {
 
-	//time
-	static long startTime = 0;
-	static long endTime = 0;
 
-	//timers - note milliseconds
-	private static void tick(){ //start timing
-		startTime = System.currentTimeMillis();
-	}
-	private static void tock(){ //end timing
-		endTime=System.currentTimeMillis(); 
-	}
-	//time
+    //time
+    static long startTime = 0;
+    static long endTime = 0;
+
+
+    //timers - note milliseconds
+    private static void tick(){ //start timing
+        startTime = System.currentTimeMillis();
+    }
+    private static void tock(){ //end timing
+        endTime=System.currentTimeMillis();
+    }
+    //time
+
 
     public static void main(String[] args) throws IOException {
         Grid simulationGrid;
+
 
         if (args.length != 2) {
             System.out.println("Incorrect number of command line arguments provided.");
             System.exit(0);
         }
 
+
         String inputFileName = args[0];
         String outputFileName = args[1];
+
 
         simulationGrid = new ParallelGrid(readArrayFromCSV(inputFileName));
         int counter = 0;
         tick();
-        if (DEBUG) {
-            System.out.printf("starting config: %d \n", counter);
-            simulationGrid.printGrid();
-        }
-        while (simulationGrid.update()) {
-            if (DEBUG) simulationGrid.printGrid();
-            counter++;
-        }
+        simulationGrid.update()
         tock();
+
 
         System.out.println("Simulation complete, writing image...");
         simulationGrid.gridToImage(outputFileName);
+
 
         System.out.printf("Number of steps to stable state: %d \n", counter);
         System.out.printf("Time: %d ms\n", endTime - startTime);
     }
 }
+
 
--- src/serialAbelianSandpile/ParallelGrid.java
+++ src/serialAbelianSandpile/ParallelGrid.java
@@ -1,8 +1,10 @@
 package serialAbelianSandpile;
 
+
 import serialAbelianSandpile.Grid;
 import java.util.concurrent.RecursiveTask;
 import java.util.concurrent.ForkJoinPool;
+
 
 /**
  * Parallel version of the Grid class using Fork/Join framework with quadrant splitting.
@@ -10,19 +12,24 @@ import java.util.concurrent.ForkJoinPool;
  */
 public class ParallelGrid extends Grid {
 
+
     private static final int THRESHOLD = 3000; // Threshold to decide when to stop splitting
+
 
     public ParallelGrid(int w, int h) {
         super(w, h);
     }
 
+
     public ParallelGrid(int[][] newGrid) {
         super(newGrid);
     }
 
+
     public ParallelGrid(Grid copyGrid) {
         super(copyGrid);
     }
+
 
     @Override
     public boolean update() {
@@ -30,14 +37,17 @@ public class ParallelGrid extends Grid {
         boolean result = pool.invoke(new UpdateTask(1, 1, getRows(), getColumns()));
         pool.shutdown();
 
+
         if (result) {
             nextTimeStep();
         }
         return result;
     }
 
+
     private class UpdateTask extends RecursiveTask<Boolean> {
         int startRow, startCol, endRow, endCol;
+
 
         UpdateTask(int startRow, int startCol, int endRow, int endCol) {
             this.startRow = startRow;
@@ -46,10 +56,12 @@ public class ParallelGrid extends Grid {
             this.endCol = endCol;
         }
 
+
         @Override
         protected Boolean compute() {
             int rows = endRow - startRow + 1;
             int cols = endCol - startCol + 1;
+
 
             // If the grid segment is small enough, process it sequentially
             if (rows * cols <= THRESHOLD) {
@@ -59,10 +71,12 @@ public class ParallelGrid extends Grid {
                 int midRow = (startRow + endRow) / 2;
                 int midCol = (startCol + endCol) / 2;
 
+
                 UpdateTask task1 = new UpdateTask(startRow, startCol, midRow, midCol); // Top-left
                 UpdateTask task2 = new UpdateTask(startRow, midCol + 1, midRow, endCol); // Top-right
                 UpdateTask task3 = new UpdateTask(midRow + 1, startCol, endRow, midCol); // Bottom-left
                 UpdateTask task4 = new UpdateTask(midRow + 1, midCol + 1, endRow, endCol); // Bottom-right
+
 
                 // Fork three tasks and compute one directly
                 task1.fork();
@@ -70,17 +84,21 @@ public class ParallelGrid extends Grid {
                 task3.fork();
                 boolean result4 = task4.compute();
 
+
                 // Join the results of the other three tasks
                 boolean result1 = task1.join();
                 boolean result2 = task2.join();
                 boolean result3 = task3.join();
 
+
                 return result4 || result1 || result2 || result3;
             }
         }
 
+
         private boolean updateSegment() {
             boolean change = false;
+
 
             for (int i = startRow; i <= endRow; i++) {
                 for (int j = startCol; j <= endCol; j++) {
@@ -90,11 +108,13 @@ public class ParallelGrid extends Grid {
                             (grid[i][j - 1] / 4) +
                             (grid[i][j + 1] / 4);
 
+
                     if (grid[i][j] != updateGrid[i][j]) {
                         change = true;
                     }
                 }
             }
+
 
             return change;
         }
